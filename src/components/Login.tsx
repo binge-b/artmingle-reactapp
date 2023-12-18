8e22b742bf02cae46e1c2638dff7f74929c16b70
binge-b <jtqbk3670@gmail.com>
2023-12-19 02:54:40 +0900

    デプロイトライ

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -18,17 +18,17 @@ const Login = () => {
   return (
     <div className="form-container flex items-center justify-center h-screen bg-gray-200">
         <div className="bg-white p-6 rounded-lg shadow-lg w-7/8">
-        <h1 className="text-2xl font-bold mb-6 text-center">ログインフォーム</h1>
+        <h1 className="text-2xl font-bold mb-6 text-center">Log in Form</h1>
         <form onSubmit={handleSubmit(onSubmit)}>
-            <label htmlFor="名前" className="block text-sm mb-2">名前</label>
+            <label htmlFor="名前" className="block text-sm mb-2">Name</label>
             <input id="name" type="text" {...register("name", { required: "名前は必須です。", minLength: {value: 6, message: "6文字以上で入力してください。"}})} className="w-full p-2 border-2 border-gray-300 rounded-md mb-2"/>
             <p className="text-xs text-red-600 text-end mb-3 p-2">{errors.name?.message as React.ReactNode}</p>
 
-            <label htmlFor="メールアドレス" className="block text-sm mb-2">メールアドレス</label>
+            <label htmlFor="メールアドレス" className="block text-sm mb-2">Email</label>
             <input id="email" type="email"{...register("email", { required: "emailは必須です。"})} className="w-full p-2 border-2 border-gray-300 rounded-md mb-2"/>
             <p className="text-xs text-red-600 text-end mb-3 p-2">{errors.email?.message as React.ReactNode}</p>
 
-            <label htmlFor="パスワード" className="block text-sm mb-2">パスワード</label>
+            <label htmlFor="パスワード" className="block text-sm mb-2">Password</label>
             <input id="password" type="password" {...register("password", { required: "パスワードは必須です。", minLength: {value: 8, message: "8文字以上で入力してください。"} })} className="w-full p-2 border-2 border-gray-300 rounded-md mb-2"/>
             <p className="text-xs text-red-600 text-end mb-3 p-2">{errors.password?.message as React.ReactNode}</p>
 
